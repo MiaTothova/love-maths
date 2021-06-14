acfc0700867b64d33389b34e890398a7de60d4ff
MiaTothova <21tothova@gmail.com>
2021-06-14 21:32:45 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,14 +10,30 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert ("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
 
+    runGame("addition");
+
 })
 
-function runGame() {
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
+function runGame(gameType) {
+    //Creates 2 random numbers between 1 and 25
+    let num1 = Math.floor(Math.random()* 25);Math.floor(Math.random()* 25) + 1;
+    let num2 = Math.floor(Math.random()* 25);Math.floor(Math.random()* 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 
 }
 
@@ -37,7 +53,12 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
+    
+
 
 }
 
