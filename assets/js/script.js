71fa1ac8f02fc6ff1305e5ca99ef69f23bb645ac
MiaTothova <21tothova@gmail.com>
2021-06-14 21:54:23 +0000

    ....

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,7 +41,23 @@ function checkAnswer() {
 
 }
 
+/**
+ * Gets the operands (the numbers) and the operator (plus, minus ect)
+ * girectly from the dom, and returns the correct answer.
+ */
 function calculateCorrectAnswer() {
+    let operand1 = parseInt(document.getElementById('operand1').innerText)
+    let operand2 = parseInt(document.getElementById('operand2').innerText)
+    let operator = document.getElementById("operator").innerText;
+
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`;
+
+    }  
+    }
 
 }
 
