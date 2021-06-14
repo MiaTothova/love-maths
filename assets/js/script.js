9ce830d919b7e1e863f0edb985135253a63a1be4
MiaTothova <21tothova@gmail.com>
2021-06-14 18:42:10 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
+
+
+
